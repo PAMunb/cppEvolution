46f90a2e8d7312148be74123f25e8f5bc20b4258
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-05-03 00:00:36 -0300

    Add a gitginore
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/main/java/br/unb/cic/cpp/evolution/model/Observations.java
@@ -0,0 +1,68 @@
+package br.unb.cic.cpp.evolution.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SummaryOfObservations {
+
+    private String project;
+    private String revision;
+    private Date date;
+    private long numberOfLambdaExpressions;
+    private long numberOfAutoDeclarations;
+    private long numberOfDeclType;
+    private long numberOfForRangeStatements;
+    private long numberOfConstExpressions;
+    private long numberOfIfWithInitializerStatements;
+    private long numberOfThreadDeclarations;
+    private long numberOfFutureDeclarations;
+    private long numberOfSharedFutureDeclarations;
+    private long numberOfPromiseDeclarations;
+    private long numberOfAsync;
+    private long numberOfClassDeclarations;
+    private long numberOfStatements;
+    private long files;
+    private long[] errors = new long[] {0, 0, 0};
+    private long elapsedTime;
+
+    public String toString() {
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        return    project + ","
+                + simpleDateFormat.format(date) + ","
+                + revision + ","
+                + files + ","
+                + numberOfLambdaExpressions + ","
+                + numberOfAutoDeclarations  + ","
+                + numberOfDeclType + ","
+                + numberOfForRangeStatements + ","
+                + numberOfConstExpressions + ","
+                + numberOfIfWithInitializerStatements + ","
+                + numberOfThreadDeclarations + ","
+                + numberOfFutureDeclarations + ","
+                + numberOfSharedFutureDeclarations + ","
+                + numberOfPromiseDeclarations + ","
+                + numberOfAsync + ","
+                + numberOfClassDeclarations + ","
+                + numberOfStatements + ","
+                + errors[0] + ","
+                + errors[1] + ","
+                + errors[2] + ","
+                + elapsedTime;
+    }
+
+    public void setError(final int i, final int value) {
+        if (i > 3 || i < 0) {
+            return;
+        }
+
+        errors[i] = value;
+    }
+}
--- src/main/java/br/unb/cic/cpp/evolution/parser/CPlusPlusParser.java
+++ src/main/java/br/unb/cic/cpp/evolution/parser/CPlusPlusParser.java
@@ -1,5 +1,7 @@
 package br.unb.cic.cpp.evolution.parser;
 
+import lombok.NoArgsConstructor;
+import lombok.val;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.gnu.cpp.GPPLanguage;
 import org.eclipse.cdt.core.index.IIndex;
@@ -10,17 +12,20 @@ import org.eclipse.core.runtime.CoreException;
 import java.util.HashMap;
 import java.util.Map;
 
+@NoArgsConstructor
 public class CPPParser {
 
     public IASTTranslationUnit parse(String content) throws CoreException {
-        FileContent fc = FileContent.create("", content.toCharArray());
-        Map<String, String> macroDefinitions = new HashMap<>();
-        String[] includeSearchPaths = new String[0];
-        IScannerInfo si = new ScannerInfo(macroDefinitions, includeSearchPaths);
-        IncludeFileContentProvider icfp = IncludeFileContentProvider.getEmptyFilesProvider();
+        val fc = FileContent.create("", content.toCharArray());
+        val macroDefinitions = new HashMap<String, String>();
+        val includeSearchPaths = new String[0];
+        val si = new ScannerInfo(macroDefinitions, includeSearchPaths);
+        val icfp = IncludeFileContentProvider.getEmptyFilesProvider();
+        val options = ILanguage.OPTION_IS_SOURCE_UNIT;
+
         IIndex idx = null;
-        int options = ILanguage.OPTION_IS_SOURCE_UNIT;
         IParserLogService log = new DefaultLogService();
+
         return GPPLanguage.getDefault().getASTTranslationUnit(fc, si, icfp, idx, options, log);
     }
 }
