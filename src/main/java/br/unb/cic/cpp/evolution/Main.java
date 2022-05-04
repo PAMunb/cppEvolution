380e8896f33df68cc35921587d6064397706dfac
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-05-03 23:07:07 -0300

    Add verification for non compliant execution
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/main/java/br/unb/cic/cpp/evolution/Main.java
+++ src/main/java/br/unb/cic/cpp/evolution/Main.java
@@ -4,16 +4,21 @@ import br.unb.cic.cpp.evolution.git.RepositoryWalker;
 import br.unb.cic.cpp.evolution.io.CSVUtil;
 import br.unb.cic.cpp.evolution.io.FileUtil;
 import lombok.val;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class Main {
 
-    public static void main(final String[] args) {
-        val logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(final String[] args) {
         try {
+            if (args.length == 0 || args[0].isEmpty()) {
+                usage();
+            }
+
             val path = args[0];
             val f = new File(path);
 
@@ -41,5 +46,11 @@ public class Main {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void usage() {
+        logger.error("java -jar cpp-evolution.jar <path>\n");
+        logger.error("\nArguments\n");
+        logger.error("<path> - The path to a cplusplus git repository");
     }
 }
