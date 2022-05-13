92befa09610aba929b492f1a0624056dc9b148f1
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-05-13 14:40:26 -0300

    format the code

--- src/main/java/br/unb/cic/cpp/evolution/Main.java
+++ src/main/java/br/unb/cic/cpp/evolution/Main.java
@@ -15,52 +15,54 @@ import lombok.val;
 
 public class Main {
 
-	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-	public static void main(final String[] args) {
-		if (args.length == 0 || args[0].isEmpty()) {
-			usage();
-			System.exit(1);
-		}
+    public static void main(final String[] args) {
+        if (args.length == 0 || args[0].isEmpty()) {
+            usage();
+            System.exit(1);
+        }
 
-		val path = args[0];
-		val f = new File(path);
+        val path = args[0];
+        val f = new File(path);
 
-		if (f.exists() && f.isDirectory()) {
-			val repositories = f.listFiles(File::isDirectory);
+        if (f.exists() && f.isDirectory()) {
+            val repositories = f.listFiles(File::isDirectory);
 
-			try {
-				val csv = new FileCSV(f.getAbsolutePath() + "/../out/results.csv");
-				if (repositories != null) {
-					val cores = Runtime.getRuntime().availableProcessors();
-					val pool = Executors.newFixedThreadPool(cores + 1);
+            try {
+                val csv = new FileCSV(f.getAbsolutePath() + "/../out/results.csv");
+                if (repositories != null) {
+                    val cores = Runtime.getRuntime().availableProcessors();
+                    val pool = Executors.newFixedThreadPool(cores + 1);
 
                     val futures = new ArrayList<Future>();
 
-					for (File repository : repositories) {
-						val outputFile = f.getAbsolutePath() + "/../out/" + repository.getName() + ".md";
-						val walker= RepositoryWalkerTask.builder()
+                    for (File repository : repositories) {
+                        val outputFile = f.getAbsolutePath() + "/../out/" + repository.getName() + ".md";
+                        val walker = RepositoryWalkerTask.builder()
                                 .csv(csv)
                                 .repositoryName(repository.getName())
-								.repositoryPath(repository.getAbsolutePath()).repositoryObservationsFile(outputFile)
-								.build();
+                                .repositoryPath(repository.getAbsolutePath()).repositoryObservationsFile(outputFile)
+                                .build();
 
-						futures.add(pool.submit(walker));
-					}
+                        futures.add(pool.submit(walker));
+                    }
                     // We have to synchronize all threads before a call to csv.close()
-                    for(Future future: futures) { future.get(); }
+                    for (Future future : futures) {
+                        future.get();
+                    }
 
-					csv.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                    csv.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public static void usage() {
-		logger.error("java -jar cpp-evolution.jar <path>\n");
-		logger.error("\nArguments\n");
-		logger.error("<path> - The path to a/set of git repository(ies) containing c++ code");
-	}
+    public static void usage() {
+        logger.error("java -jar cpp-evolution.jar <path>\n");
+        logger.error("\nArguments\n");
+        logger.error("<path> - The path to a/set of git repository(ies) containing c++ code");
+    }
 }
