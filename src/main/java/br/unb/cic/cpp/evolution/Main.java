58e41a01f01547923d4fbf0507b80366fe3c2f29
waltim <waltimlmm@gmail.com>
2022-06-16 10:21:21 -0300

    enable program to execute in Windows or Linux, create new walk method to compute metrics for list of commits and add project name parameter to args

--- src/main/java/br/unb/cic/cpp/evolution/Main.java
+++ src/main/java/br/unb/cic/cpp/evolution/Main.java
@@ -3,8 +3,10 @@ package br.unb.cic.cpp.evolution;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -18,80 +20,88 @@ import lombok.val;
 
 public class Main {
 
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	private static String project = null;
 
-    public static void main(final String[] args) throws Exception {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-        int threads = 0;
+	public static void main(final String[] args) throws Exception {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		int threads = 0;
 
-        Date initialDate = formatter.parse("01-01-2010");
-        Date finalDate = Calendar.getInstance().getTime();
-        int step = 7;
+		Date initialDate = formatter.parse("01-01-2010");
+		Date finalDate = Calendar.getInstance().getTime();
+		int step = 7;
 
-        if (args.length == 0 || args[0].isEmpty()) {
-            usage();
-            System.exit(1);
-        }
-        for(int i = 1; i < args.length; i++) {
-            if (args[i].startsWith("--threads")) {
-                threads = Integer.parseInt(args[i].replace("--threads=", ""));
-            } else if (args[i].startsWith("--date-init=")) {
-                initialDate = formatter.parse(args[i].replace("--date-init=", ""));
-            } else if (args[i].startsWith("--date-end=")) {
-                finalDate = formatter.parse(args[i].replace("--date-end=", ""));
-            } else if (args[i].startsWith("--step=")) {
-                step = Integer.parseInt(args[i].replace("--step=", ""));
-            }
-        }
+		if (args.length == 0 || args[0].isEmpty()) {
+			usage();
+			System.exit(1);
+		}
+		for (int i = 1; i < args.length; i++) {
+			if (args[i].startsWith("--threads")) {
+				threads = Integer.parseInt(args[i].replace("--threads=", ""));
+			} else if (args[i].startsWith("--date-init=")) {
+				initialDate = formatter.parse(args[i].replace("--date-init=", ""));
+			} else if (args[i].startsWith("--date-end=")) {
+				finalDate = formatter.parse(args[i].replace("--date-end=", ""));
+			} else if (args[i].startsWith("--step=")) {
+				step = Integer.parseInt(args[i].replace("--step=", ""));
+			} else if (args[i].startsWith("--project=")) {
+				project = args[i].replace("--project=", "");
+			}
+		}
 
-        logger.info("Init date {}", initialDate.toString());
-        logger.info("End date {}", finalDate.toString());
-        logger.info("Step {}", step);
+		logger.info("Init date {}", initialDate.toString());
+		logger.info("End date {}", finalDate.toString());
+		logger.info("Step {}", step);
 
+		val path = args[0];
+		val f = new File(path);
 
-        val path = args[0];
-        val f = new File(path);
+		if (f.exists() && f.isDirectory()) {
 
-        if (f.exists() && f.isDirectory()) {
-            val repositories = f.listFiles(File::isDirectory);
+			File[] repositories = null;
+			if (project == null) {
+				repositories = f.listFiles(File::isDirectory);
+			} else {
+				repositories = f.listFiles(File::isDirectory);
+				repositories = Arrays.stream(repositories).filter(t -> t.getName().equals(project))
+						.toArray(File[]::new);
+			}
+			try {
+				val csv = new FileCSV(f.getAbsolutePath() + osValidation.osBarLine() + ".." + osValidation.osBarLine()
+						+ "out" + osValidation.osBarLine() + "results.csv");
+				if (repositories != null) {
+					val cores = threads == 0 ? Runtime.getRuntime().availableProcessors() : threads;
+					val pool = Executors.newFixedThreadPool(cores);
 
-            try {
-                val csv = new FileCSV(f.getAbsolutePath() + "/../out/results.csv");
-                if (repositories != null) {
-                    val cores = threads == 0? Runtime.getRuntime().availableProcessors() : threads;
-                    val pool = Executors.newFixedThreadPool(cores);
+					val futures = new ArrayList<Future>();
 
-                    val futures = new ArrayList<Future>();
+					for (File repository : repositories) {
+						val outputFile = f.getAbsolutePath() + osValidation.osBarLine() + ".."
+								+ osValidation.osBarLine() + "out" + osValidation.osBarLine() + repository.getName()
+								+ ".md";
+						val walker = RepositoryWalkerTask.builder().csv(csv).repositoryName(repository.getName())
+								.repositoryPath(repository.getAbsolutePath()).repositoryObservationsFile(outputFile)
+								.initDate(initialDate).endDate(finalDate).step(step).build();
 
-                    for (File repository : repositories) {
-                        val outputFile = f.getAbsolutePath() + "/../out/" + repository.getName() + ".md";
-                        val walker = RepositoryWalkerTask.builder()
-                                .csv(csv)
-                                .repositoryName(repository.getName())
-                                .repositoryPath(repository.getAbsolutePath()).repositoryObservationsFile(outputFile)
-                                .initDate(initialDate)
-                                .endDate(finalDate)
-                                .step(step)
-                                .build();
-
-                        futures.add(pool.submit(walker));
-                    }
-                    // We have to synchronize all threads before a call to csv.close()
-                    for (Future future : futures) {
-                        future.get();
-                    }
+						futures.add(pool.submit(walker));
+					}
+					// We have to synchronize all threads before a call to csv.close()
+					for (Future future : futures) {
+						future.get();
+					}
 					pool.shutdown();
-                    csv.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+					csv.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    public static void usage() {
-        logger.error("java -jar cpp-evolution.jar <path>\n");
-        logger.error("\nArguments\n");
-        logger.error("<path> - The path to a/set of git repository(ies) containing c++ code");
-    }
+	public static void usage() {
+		logger.error("java -jar cpp-evolution.jar <path>\n");
+		logger.error("\nArguments\n");
+		logger.error("<path> - The path to a/set of git repository(ies) containing c++ code");
+	}
+
 }
--- src/main/java/br/unb/cic/cpp/evolution/git/RepositoryWalker.java
+++ src/main/java/br/unb/cic/cpp/evolution/git/RepositoryWalker.java
@@ -1,6 +1,6 @@
 package br.unb.cic.cpp.evolution.git;
 
-
+import br.unb.cic.cpp.evolution.osValidation;
 import br.unb.cic.cpp.evolution.io.FileUtil;
 import br.unb.cic.cpp.evolution.model.Observation;
 import br.unb.cic.cpp.evolution.model.Observations;
@@ -10,11 +10,17 @@ import lombok.val;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.CheckoutConflictException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRefNameException;
+import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,157 +32,247 @@ import java.util.concurrent.TimeUnit;
 
 public class RepositoryWalker {
 
-    private int totalCommits = 0;
+	private int totalCommits = 0;
 
-    private final String project;
-    private final String path;
+	private final String project;
+	private final String path;
 
-    private final Repository repository;
-    private final Set<Observation> observations;
-    private final List<Observations> summary;
+	private final Repository repository;
+	private final Set<Observation> observations;
+	private final List<Observations> summary;
+	private List<String> commits;
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public RepositoryWalker(String project, String path) throws Exception {
-        this.project = project;
-        this.path = path;
+	public RepositoryWalker(String project, String path, List<String> commits) throws Exception {
+		this(project, path);
+		this.commits = commits;
+	}
 
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+	public RepositoryWalker(String project, String path) throws Exception {
+		this.project = project;
+		this.path = path;
 
-        repository = builder.setGitDir(new File(path + "/.git")).readEnvironment().findGitDir().build();
-        observations = new HashSet<>();
-        summary = new ArrayList<>();
-    }
+		FileRepositoryBuilder builder = new FileRepositoryBuilder();
 
-    public void walk(Date initDate, Date endDate, int step) throws Exception {
-        logger.info("processing project {}", project);
+		repository = builder.setGitDir(new File(path + osValidation.osBarLine() + ".git")).readEnvironment()
+				.findGitDir().build();
+		observations = new HashSet<>();
+		summary = new ArrayList<>();
+	}
 
-        Date previous = null;
+	public void walk() throws Exception {
+
+		for (String cmt : commits) {
+
+			Git git = new Git(repository);
+
+			Long start = System.currentTimeMillis();
+
+			ObjectId head = repository.resolve(Constants.HEAD);
+
+			RevWalk walk = new RevWalk(repository);
+			ObjectId obj = repository.resolve(cmt);
+			RevCommit commit = walk.parseCommit(obj);
+
+			git.checkout().setName(commit.getName()).call();
+
+			Observations commitSummary = new Observations();
+
+			commitSummary.setProject(project);
+			commitSummary.setDate(commit.getAuthorIdent().getWhen());
+			commitSummary.setRevision(commit.getName());
+
+			int genericError = 0;
+			int ioError = 0;
+			int parserError = 0;
+
+			val visitor = new MetricsVisitor();
+			val files = FileUtil.listFiles(this.path);
+
+			for (File f : files) {
+				try {
+					CPlusPlusParser parser = new CPlusPlusParser();
+					IASTTranslationUnit unit = parser.parse(FileUtil.readContent(f));
+					unit.accept(visitor);
+				} catch (IOException e) {
+					ioError++;
+				} catch (CoreException e) {
+					parserError++;
+				} catch (Throwable t) {
+					genericError++;
+				}
+			}
+
+			git.checkout().setName(head.getName()).call();
+
+			observations.addAll(visitor.getObservations());
+
+			commitSummary.setNumberOfLambdaExpressions(visitor.getLambdaExpressions());
+			commitSummary.setNumberOfAutoDeclarations(visitor.getAuto());
+			commitSummary.setNumberOfDeclType(visitor.getDeclType());
+			commitSummary.setNumberOfForRangeStatements(visitor.getRangeForStatement());
+			commitSummary.setNumberOfConstExpressions(visitor.getConstExpr());
+			commitSummary.setNumberOfIfWithInitializerStatements(visitor.getIfStatementWithInitializer());
+			commitSummary.setFiles(files.size());
+			commitSummary.setNumberOfThreadDeclarations(visitor.getThreadDeclarations());
+			commitSummary.setNumberOfSharedFutureDeclarations(0);
+			commitSummary.setNumberOfPromiseDeclarations(visitor.getPromiseDeclarations());
+			commitSummary.setNumberOfAsync(0);
+			commitSummary.setNumberOfClassDeclarations(0);
+			commitSummary.setNumberOfStatements(visitor.getStatements());
+			commitSummary.setError(0, ioError);
+			commitSummary.setError(1, parserError);
+			commitSummary.setError(2, genericError);
+			commitSummary.setElapsedTime(System.currentTimeMillis() - start);
+
+			this.summary.add(commitSummary);
+
+			Collections.sort(summary, new Comparator<Observations>() {
+				public int compare(Observations o1, Observations o2) {
+					return o1.getDate().compareTo(o2.getDate());
+				}
+			});
+
+			walk.close();
+			git.close();
+		}
+
+	}
+
+	public void walk(Date initDate, Date endDate, int step) throws Exception {
+		logger.info("processing project {}", project);
+
+		Date previous = null;
 
 //        RevWalk walk = new RevWalk(repository);
 //        walk.markStart(heads());
 //
-        ObjectId head = repository.resolve(Constants.HEAD);
+		ObjectId head = repository.resolve(Constants.HEAD);
 //
 //        walk.sort(RevSort.TOPO, true);
 //        walk.sort(RevSort.COMMIT_TIME_DESC, true);
 
-        Git git = new Git(repository);
+		Git git = new Git(repository);
 
-        String treeName = "refs/heads/master";
+		String treeName = "refs/heads/master";
 
-        HashMap<Date, ObjectId> commits = new HashMap<>();
-        List<Date> commitDates = new ArrayList<>();
+		HashMap<Date, ObjectId> commits = new HashMap<>();
+		List<Date> commitDates = new ArrayList<>();
 
-        for(RevCommit c: git.log().add(repository.resolve(treeName)).call()) {
-            PersonIdent author = c.getAuthorIdent();
-            Date current = author.getWhen();
-            if(current.compareTo(initDate) >= 0 && current.compareTo(endDate) <= 0) {
-                commitDates.add(current);
-                commits.put(current, c.toObjectId());
-            }
-        }
-        Collections.sort(commitDates);
-        long total = commitDates.size();
-        logger.info("Number of commits {} ", commits.size());
+		for (RevCommit c : git.log().add(repository.resolve(treeName)).call()) {
+			PersonIdent author = c.getAuthorIdent();
+			Date current = author.getWhen();
+			if (current.compareTo(initDate) >= 0 && current.compareTo(endDate) <= 0) {
+				commitDates.add(current);
+				commits.put(current, c.toObjectId());
+			}
+		}
 
-        //Collections.reverse(commitDates);
-        //int max = 10;
-        long traversed = 0;
-        for(Date current: commitDates) {
-            if(traversed % 500 == 0) {
-                logger.info(" - {}: visiting commit {} of {}",  project, traversed, total);
-            }
-            traversed++;
-            if(previous == null || (diffInDays(previous, current) >= step)) {
+		Collections.sort(commitDates);
+		long total = commitDates.size();
+		logger.info("Number of commits {} ", commits.size());
 
-                collectMetrics(head, current, commits);
-                previous = current;
-                totalCommits++;
+		// Collections.reverse(commitDates);
+		// int max = 10;
+		long traversed = 0;
+		for (Date current : commitDates) {
+			if (traversed % 500 == 0) {
+				logger.info(" - {}: visiting commit {} of {}", project, traversed, total);
+			}
+			traversed++;
+			if (previous == null || (diffInDays(previous, current) >= step)) {
+
+				collectMetrics(head, current, commits);
+				previous = current;
+				totalCommits++;
 //                max--;
 //                if(max == 0) break;
-            }
-        }
-    }
+			}
+		}
+	}
 
-    private void collectMetrics(ObjectId head, Date current, HashMap<Date, ObjectId> commits) throws Exception {
-        Long start = System.currentTimeMillis();
-        ObjectId id = commits.get(current);
+	private void collectMetrics(ObjectId head, Date current, HashMap<Date, ObjectId> commits) throws Exception {
+		Long start = System.currentTimeMillis();
+		ObjectId id = commits.get(current);
 
-        RevCommit commit = repository.parseCommit(id);
+		RevCommit commit = repository.parseCommit(id);
 
-        Observations commitSummary = new Observations();
+		Observations commitSummary = new Observations();
 
-        commitSummary.setProject(project);
-        commitSummary.setDate(commit.getAuthorIdent().getWhen());
-        commitSummary.setRevision(id.name());
+		commitSummary.setProject(project);
+		commitSummary.setDate(commit.getAuthorIdent().getWhen());
+		commitSummary.setRevision(id.name());
 
-        Git git = new Git(repository);
+		Git git = new Git(repository);
 
-        git.checkout().setName(id.getName()).call();
+		git.checkout().setName(id.getName()).call();
 
-        int genericError = 0;
-        int ioError = 0;
-        int parserError = 0;
+		int genericError = 0;
+		int ioError = 0;
+		int parserError = 0;
 
-        val visitor = new MetricsVisitor();
-        val files = FileUtil.listFiles(this.path);
+		val visitor = new MetricsVisitor();
+		val files = FileUtil.listFiles(this.path);
 
-        for(File f: files) {
-            try {
-                CPlusPlusParser parser = new CPlusPlusParser();
-                IASTTranslationUnit unit = parser.parse(FileUtil.readContent(f));
-                unit.accept(visitor);
-            } catch(IOException e) {
-                ioError++;
-            } catch(CoreException e) {
-                parserError++;
-            } catch(Throwable t) {
-                genericError++;
-            }
-        }
+		for (File f : files) {
+			try {
+				CPlusPlusParser parser = new CPlusPlusParser();
+				IASTTranslationUnit unit = parser.parse(FileUtil.readContent(f));
+				unit.accept(visitor);
+			} catch (IOException e) {
+				ioError++;
+			} catch (CoreException e) {
+				parserError++;
+			} catch (Throwable t) {
+				genericError++;
+			}
+		}
 
-        git.checkout().setName(head.getName()).call();
+		git.checkout().setName(head.getName()).call();
 
-        observations.addAll(visitor.getObservations());
+		observations.addAll(visitor.getObservations());
 
-        commitSummary.setNumberOfLambdaExpressions(visitor.getLambdaExpressions());
-        commitSummary.setNumberOfAutoDeclarations(visitor.getAuto());
-        commitSummary.setNumberOfDeclType(visitor.getDeclType());
-        commitSummary.setNumberOfForRangeStatements(visitor.getRangeForStatement());
-        commitSummary.setNumberOfConstExpressions(visitor.getConstExpr());
-        commitSummary.setNumberOfIfWithInitializerStatements(visitor.getIfStatementWithInitializer());
-        commitSummary.setFiles(files.size());
-        commitSummary.setNumberOfThreadDeclarations(visitor.getThreadDeclarations());
-        commitSummary.setNumberOfSharedFutureDeclarations(0);
-        commitSummary.setNumberOfPromiseDeclarations(visitor.getPromiseDeclarations());
-        commitSummary.setNumberOfAsync(0);
-        commitSummary.setNumberOfClassDeclarations(0);
-        commitSummary.setNumberOfStatements(visitor.getStatements());
-        commitSummary.setError(0, ioError);
-        commitSummary.setError(1, parserError);
-        commitSummary.setError(2, genericError);
-        commitSummary.setElapsedTime(System.currentTimeMillis() - start);
+		commitSummary.setNumberOfLambdaExpressions(visitor.getLambdaExpressions());
+		commitSummary.setNumberOfAutoDeclarations(visitor.getAuto());
+		commitSummary.setNumberOfDeclType(visitor.getDeclType());
+		commitSummary.setNumberOfForRangeStatements(visitor.getRangeForStatement());
+		commitSummary.setNumberOfConstExpressions(visitor.getConstExpr());
+		commitSummary.setNumberOfIfWithInitializerStatements(visitor.getIfStatementWithInitializer());
+		commitSummary.setFiles(files.size());
+		commitSummary.setNumberOfThreadDeclarations(visitor.getThreadDeclarations());
+		commitSummary.setNumberOfSharedFutureDeclarations(0);
+		commitSummary.setNumberOfPromiseDeclarations(visitor.getPromiseDeclarations());
+		commitSummary.setNumberOfAsync(0);
+		commitSummary.setNumberOfClassDeclarations(0);
+		commitSummary.setNumberOfStatements(visitor.getStatements());
+		commitSummary.setError(0, ioError);
+		commitSummary.setError(1, parserError);
+		commitSummary.setError(2, genericError);
+		commitSummary.setElapsedTime(System.currentTimeMillis() - start);
 
-        this.summary.add(commitSummary);
-    }
+		this.summary.add(commitSummary);
+	}
 
-    public long diffInDays(Date previous, Date current) {
-        long diff = Math.abs(previous.getTime() - current.getTime());
-        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-    }
+	public long diffInDays(Date previous, Date current) {
+		long diff = Math.abs(previous.getTime() - current.getTime());
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
 
-    private List<RevCommit> heads() throws IOException {
-        val commits = new ArrayList<RevCommit>();
-        val head = repository.resolve("HEAD");
+	private List<RevCommit> heads() throws IOException {
+		val commits = new ArrayList<RevCommit>();
+		val head = repository.resolve("HEAD");
 
-        commits.add(repository.parseCommit(head));
+		commits.add(repository.parseCommit(head));
 
-        return commits;
-    }
+		return commits;
+	}
 
-    public List<Observations> getSummary() {
-        return summary;
-    }
-    public Set<Observation> getObservations() { return observations; }
+	public List<Observations> getSummary() {
+		return summary;
+	}
+
+	public Set<Observation> getObservations() {
+		return observations;
+	}
 }
